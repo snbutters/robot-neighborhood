b82cb389215d6decfb4b479237cb7b76d9da3877
Samantha Butters <61861742+snbutters@users.noreply.github.com>
2020-05-16 23:26:36 -0700

    update javascript to begin adding back-end functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
+// Business Logic (back-end)
+
+const beepBoop = function(number) {
+  const result = [];
+  
+
+
+
+
+// jQuery User Interface Logic
+
 $(document).ready(function() {
   $("form#beep-boop").submit(function(event) {
     event.preventDefault();
     const number = parseInt($("input#number").val());
+
+
+    $("#result").show();
+  });
+});
