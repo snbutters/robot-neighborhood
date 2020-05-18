5cdf796ce2904ed4da32c07b9f91718578dfe7e1
Samantha Butters <61861742+snbutters@users.noreply.github.com>
2020-05-17 19:02:40 -0700

    add functionality to javascript file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 // Business Logic (back-end)
 
 const beepBoop = function(number) {
-  const result = [];
-  
+  const array = [];
+  for (i = 0; i <= number; i++) {
+    let input = i.toString();
 
-
-
+    if (input.includes("1")) {
+      array.push("Beep!");
+    } else if (input.includes("2")) {
+      array.push("boop!");
+    } else if (input.includes("3")) {
+      array.push("Won't you be my neighbor?");
+    }
+    return array;
+  }
+};
 
 // jQuery User Interface Logic
 
