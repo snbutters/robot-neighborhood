a715c07fc21029307191ad6ccc9f549d55727907
Samantha Butters <61861742+snbutters@users.noreply.github.com>
2020-05-17 20:41:25 -0700

    troubleshoot javascript code and add additional functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 // Business Logic (back-end)
 
 const beepBoop = function(number) {
-  const array = [];
-  for (i = 0; i <= number; i++) {
+  let result = [];
+  for (let i = 0; i <= number; i++) {
     let input = i.toString();
-
-    if (input.includes("1")) {
-      array.push("Beep!");
+    if (input.includes("3")) {
+      result.push(" Won't you be my neighbor?");
     } else if (input.includes("2")) {
-      array.push("boop!");
-    } else if (input.includes("3")) {
-      array.push("Won't you be my neighbor?");
+      result.push(" Boop!");
+    } else if (input.includes("1")) {
+      result.push(" Beep!");
+    } else {
+      result.push(input)
     }
-    return array;
   }
+  return result;
 };
 
 // jQuery User Interface Logic
@@ -21,9 +22,9 @@ const beepBoop = function(number) {
 $(document).ready(function() {
   $("form#beep-boop").submit(function(event) {
     event.preventDefault();
-    const number = parseInt($("input#number").val());
-
-
+    const number = $("input#number").val();
+    let result = beepBoop(number);
+    $("#output").text(result);
     $("#result").show();
   });
 });
